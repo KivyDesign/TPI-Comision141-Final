e963cf6f6c8581e51b43aec413a0dc7833c824a9
Guido Sosa <todoelectrosolucion@gmail.com>
2023-11-17 17:51:00 -0300

    Actualizo

--- src/main/java/org/comision141/App.java
+++ src/main/java/org/comision141/App.java
@@ -2,6 +2,6 @@ package org.comision141;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hola Mundo!" );
     }
 }
